f4ab13d88e1969f28034c303b806ee60a0b8d03d
shuro4ka <zzlesia77@gmail.com>
2022-06-18 18:44:46 +0100

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
 
+stopButton.disabled = true;
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    body.style.background = getRandomHexColor(); 
+    stopButton.disabled = false;
+    timerId = setInterval(() => {
+       body.style.background = getRandomHexColor(); 
+    }, 1000);      
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;  
+}
