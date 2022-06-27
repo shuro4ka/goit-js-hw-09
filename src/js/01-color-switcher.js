9ce83ab0fcf895865ab7dd6730e7989a9d865f5f
shuro4ka <zzlesia77@gmail.com>
2022-06-27 20:21:05 +0100

    fix button

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,14 @@
 const startButton = document.querySelector("button[data-start]");
 const stopButton = document.querySelector("button[data-stop]");
 const body = document.querySelector("body");
-
+let timerId = 0;
 stopButton.disabled = true;
 
 startButton.addEventListener('click', () => {
     startButton.disabled = true;
     body.style.background = getRandomHexColor(); 
     stopButton.disabled = false;
+    
     timerId = setInterval(() => {
        body.style.background = getRandomHexColor(); 
     }, 1000);      
